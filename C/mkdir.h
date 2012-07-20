d94fd4fc6939d3391b82640d3f068d48e2b3c3fe
Darcy Bras da Silva <dardevelin.cidadecool.com>
2012-07-20 09:46:15 +0100

    add mkdir macro for system independece on making directories

---
+++ C/mkdir.h
@@ -0,0 +1,35 @@
+/*
+ * mkdir.h
+ * This file is part of snippet_collections
+ *
+ * Copyright (C) 2012 - Darcy Brás da Silva
+ *
+ * dbsglinked is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dbsglinked is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dbsglinked. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if (defined _WIN32 || _WIN64 )
+#include <conio.h>
+#include <process.h>
+#include <dir.h>
+#define MKDIR(x) mkdir(x)
+
+#else
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#define PERMISSION_FLAG 0777
+#define MKDIR(x) mkdir(x,PERMISSION_FLAG)
+
+#endif
+
