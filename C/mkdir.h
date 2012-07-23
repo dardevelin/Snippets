b6eb61cf1217ae365c1e9c98c7f4c49fe84ef17a
Darcy Brás da Silva <dardevelin@cidadecool.com>
2012-07-23 13:35:36 +0200

    fix gpl header

--- C/mkdir.h
+++ C/mkdir.h
@@ -4,18 +4,18 @@
  *
  * Copyright (C) 2012 - Darcy Brás da Silva
  *
- * dbsglinked is free software; you can redistribute it and/or modify
+ * MACRO_MKDIR is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * dbsglinked is distributed in the hope that it will be useful,
+ * MACRO_MKDIR is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with dbsglinked. If not, see <http://www.gnu.org/licenses/>.
+ * along with MACRO_MKDIR. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #if (defined _WIN32 || _WIN64 )
