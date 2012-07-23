831984fcd47337c24f89d5416c0fa32ff5f315f0
Darcy Brás da Silva <dardevelin@cidadecool.com>
2012-07-23 13:33:58 +0200

    fix GPL header

--- C/getch.c
+++ C/getch.c
@@ -4,12 +4,12 @@
  *
  * Copyright (C) 2012 - Darcy Brás da Silva
  *
- * dbsglinked is free software; you can redistribute it and/or modify
+ * getch is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * dbsglinked is distributed in the hope that it will be useful,
+ * getch is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
