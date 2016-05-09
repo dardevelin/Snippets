aea6baf4dafbbd84f8e4cfd8a15a4dab49e1ae73
Darcy Brás da Silva <dardevelin@cidadecool.com>
2016-05-09 16:57:06 +0100

    add fstrchr, function which works like strchr but over a file

---
+++ C/fstrchr.c
@@ -0,0 +1,35 @@
+/*
+ * getch.h
+ * This file is part of snippets_collection
+ *
+ * Copyright (C) 2016 - Darcy Brás da Silva
+ *
+ * getch is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * getch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with getch. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "fstrchr.h"
+
+long fstrchr(FILE **stream, const long pos, const int c)
+{
+	fseek(*stream, pos, SEEK_SET);
+	int ch = 0;
+	while(EOF != (ch = fgetc(*stream))) {
+		if( c == ch ) {
+			ungetc(ch, *stream);
+			return ftell(*stream);
+		}
+	}
+
+	return NOT_FOUND;
+}
---
+++ C/fstrchr.h
@@ -0,0 +1,40 @@
+/*
+ * getch.h
+ * This file is part of snippets_collection
+ *
+ * Copyright (C) 2016 - Darcy Brás da Silva
+ *
+ * getch is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * getch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with getch. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SNIPPET_FSTRCHR_H
+#define _SNIPPET_FSTRCHR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * this functions finds c in stream starting from pos
+ * returns the result of ftell-1 once it's found
+ * so that c can be read via fgetc
+ *
+ * if c is not found returns -1
+ *
+ * notice that the steam will not be rewinded
+ * FIXME: 
+ * add return error for EOF cases
+ */
+long fstrchr(FILE **stream, const long pos, const int c);
+
+#endif
